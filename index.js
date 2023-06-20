55802791e33243e737e68d84f6e69618a52840fa
muPoint <137110238+muPoint@users.noreply.github.com>
2023-06-20 15:43:28 +0100

    Delete index.js

--- index.js
+++
@@ -1,6 +0,0 @@
-const menuBars = document.querySelector('.menu-bars');
-const navMenu = document.querySelector('.nav-menu');
-
-menuBars.addEventListener('click', () => {
-  navMenu.classList.toggle('active');
-});
