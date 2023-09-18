79d9f8ba11bf7a453ee34906558c7fba74da3cee
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-18 06:09:29 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const burgerMenu = document.getElementById('burger-menu');
+  const menu = document.getElementById('menu');
+
+  burgerMenu.addEventListener('click', function () {
+    menu.classList.toggle('active');
+  });
+});
+
