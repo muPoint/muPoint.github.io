e7cc0b8557ef8ee5fe1d7dbbd6badc107e109366
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-18 07:01:42 +0100

    script.js

--- script.js
+++ script.js
@@ -10,24 +10,3 @@ function toggleMenu() {
         menuBtn.innerHTML = "&#10005;"; // Change to cross icon
     }
 }
-
-window.addEventListener("resize", function () {
-    var menuBtn = document.querySelector(".menu-btn");
-    var navbar = document.getElementById("navbar");
-
-    if (window.innerWidth <= 768) {
-        menuBtn.style.display = "block";
-        navbar.style.display = "none";
-    } else {
-        menuBtn.style.display = "none";
-        navbar.style.display = "block";
-    }
-});
-
-// Initialize menu state based on window width
-if (window.innerWidth <= 768) {
-    var menuBtn = document.querySelector(".menu-btn");
-    var navbar = document.getElementById("navbar");
-    menuBtn.style.display = "block";
-    navbar.style.display = "none";
-}
