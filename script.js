3cc667aa3d5c9dbe55619d70b22819bc01c406a3
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-18 07:11:28 +0100

    script.js

--- script.js
+++ script.js
@@ -2,11 +2,11 @@ function toggleMenu() {
     var navbar = document.getElementById("navbar");
     var menuBtn = document.querySelector(".menu-btn");
 
-    if (navbar.style.display === "block") {
-        navbar.style.display = "none";
+    if (navbar.classList.contains("menu-active")) {
+        navbar.classList.remove("menu-active");
         menuBtn.innerHTML = "&#9776;"; // Change back to burger icon
     } else {
-        navbar.style.display = "block";
+        navbar.classList.add("menu-active");
         menuBtn.innerHTML = "&#10005;"; // Change to cross icon
     }
 }
