b8f9395462e7e8045ea64257ddec902c2848637b
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-21 02:07:59 +0100

    script.js

--- script.js
+++ script.js
@@ -1,9 +1,47 @@
+const imageContainer = document.querySelectorAll('.image-container');
+const totalImages = imageContainer.length;
+let currentIndex = 0;
 
-/*MENU*/
-const menuBtn = document.querySelector('.menu-btn');
-const navbar = document.querySelector('.navbar');
+const currentImageElement = document.getElementById('current-image');
+const totalImagesElement = document.getElementById('total-images');
+const prevButton = document.getElementById('prev-btn');
+const nextButton = document.getElementById('next-btn');
 
-menuBtn.addEventListener('click', () => {
-    menuBtn.classList.toggle('active');
-    navbar.classList.toggle('active');
+function updateCounter() {
+    currentImageElement.textContent = currentIndex + 1;
+}
+
+function showImage(index) {
+    imageContainer.forEach((container, i) => {
+        if (i === index) {
+            container.style.display = 'block';
+        } else {
+            container.style.display = 'none';
+        }
+    });
+}
+
+prevButton.addEventListener('click', () => {
+    if (currentIndex > 0) {
+        currentIndex--;
+    } else {
+        currentIndex = totalImages - 1;
+    }
+    updateCounter();
+    showImage(currentIndex);
 });
+
+nextButton.addEventListener('click', () => {
+    if (currentIndex < totalImages - 1) {
+        currentIndex++;
+    } else {
+        currentIndex = 0;
+    }
+    updateCounter();
+    showImage(currentIndex);
+});
+
+totalImagesElement.textContent = totalImages;
+updateCounter();
+showImage(currentIndex);
+
