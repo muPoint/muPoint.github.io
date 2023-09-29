c5c5e611c027c5b0c3036c475423b54822d3abcc
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-29 16:03:12 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,65 +1,23 @@
-const images = document.querySelectorAll(".gallery-image");
-const prevArrow = document.getElementById("prev-arrow");
-const nextArrow = document.getElementById("next-arrow");
-const counter = document.getElementById("counter");
-const galleryWrapper = document.getElementById("gallery-wrapper");
 
-let currentImageIndex = 0;
-let isScrolling = false;
+/*MENU*/
+const menuBtn = document.querySelector('.menu-btn');
+const navbar = document.querySelector('.navbar');
 
-function updateCounter() {
-    counter.textContent = `${currentImageIndex + 1} / ${images.length}`;
-}
-
-prevArrow.addEventListener("click", () => {
-    if (!isScrolling) {
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        updateCounter();
-        showImage();
-    }
+menuBtn.addEventListener('click', () => {
+    menuBtn.classList.toggle('active');
+    navbar.classList.toggle('active');
 });
 
-nextArrow.addEventListener("click", () => {
-    if (!isScrolling) {
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        updateCounter();
-        showImage();
-    }
-});
 
-function showImage() {
-    images.forEach((image, index) => {
-        if (index === currentImageIndex) {
-            image.style.display = "block";
-        } else {
-            image.style.display = "none";
-        }
+/*MODEL-VIEWER*/
+function showModel(modelId) {
+    const modelViewer = document.querySelector(`#${modelId}`);
+    // Hide all models first
+    document.querySelectorAll('model-viewer').forEach(model => {
+        model.style.display = 'none';
     });
+    // Show the selected model
+    modelViewer.style.display = 'block';
 }
 
-updateCounter();
-showImage();
 
-// Enable scrolling with mouse or trackpad
-galleryWrapper.addEventListener("wheel", (e) => {
-    if (e.deltaY > 0 && !isScrolling) {
-        // Scroll down, show next image
-        currentImageIndex = (currentImageIndex + 1) % images.length;
-        updateCounter();
-        showImage();
-        disableScrollForAWhile();
-    } else if (e.deltaY < 0 && !isScrolling) {
-        // Scroll up, show previous image
-        currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
-        updateCounter();
-        showImage();
-        disableScrollForAWhile();
-    }
-});
-
-function disableScrollForAWhile() {
-    isScrolling = true;
-    setTimeout(() => {
-        isScrolling = false;
-    }, 500); // Adjust the duration as needed
-}
