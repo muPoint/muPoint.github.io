d64fcdca1793d4014dc056e52d6f7661641d9b36
muPoint <137110238+muPoint@users.noreply.github.com>
2023-09-30 12:57:52 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-/*MENU*/
+/* MENU */
 const menuBtn = document.querySelector('.menu-btn');
 const navbar = document.querySelector('.navbar');
 
@@ -8,8 +8,18 @@ menuBtn.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
 
+/* CURSOR */
+const customCursor = document.querySelector('.custom-cursor');
 
-/*MODEL-VIEWER*/
+document.addEventListener('mousemove', (e) => {
+    // Update the cursor's position
+    customCursor.style.left = `${e.clientX}px`;
+    customCursor.style.top = `${e.clientY}px`;
+});
+
+
+
+/* MODEL-VIEWER */
 function showModel(modelId) {
     const modelViewer = document.querySelector(`#${modelId}`);
     // Hide all models first
@@ -20,4 +30,44 @@ function showModel(modelId) {
     modelViewer.style.display = 'block';
 }
 
+/* GALLERY BULLETS */
 
+const galleryItems = document.querySelectorAll('.gallery-item');
+       const bullets = document.querySelectorAll('.bullet');
+       let currentIndex = 0;
+
+       // Function to show the current image
+       function showImage(index) {
+           galleryItems.forEach((item, i) => {
+               if (i === index) {
+                   item.style.display = 'block';
+               } else {
+                   item.style.display = 'none';
+               }
+           });
+           bullets.forEach((bullet, i) => {
+               bullet.classList.toggle('active', i === index);
+           });
+       }
+
+       // Initial display
+       showImage(currentIndex);
+
+       // Arrow click handlers
+       document.querySelector('.prev-arrow').addEventListener('click', () => {
+           currentIndex = (currentIndex - 1 + galleryItems.length) % galleryItems.length;
+           showImage(currentIndex);
+       });
+
+       document.querySelector('.next-arrow').addEventListener('click', () => {
+           currentIndex = (currentIndex + 1) % galleryItems.length;
+           showImage(currentIndex);
+       });
+
+       // Bullet click handlers
+       bullets.forEach((bullet, i) => {
+           bullet.addEventListener('click', () => {
+               currentIndex = i;
+               showImage(currentIndex);
+           });
+       });
